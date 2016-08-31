fef71d155ea91aa4ff12af9c63d0156633a5494d
Renn <rbiggy@gmail.com>
2016-08-31 19:48:55 +0200

    Small refactoring.

--- minesweeper/minesweeper.exs
+++ minesweeper/minesweeper.exs
@@ -4,36 +4,34 @@ defmodule Minesweeper do
   Annotate empty spots next to mines with the number of mines next to them.
   """
   @spec annotate([String.t]) :: [String.t]
+  def annotate(board) when length(board) == 0, do: board
   def annotate(board) do
-    cond do
-      length(board) == 0 -> board
-      #if there's only bombs on the board then we don't need to do anything
-      Enum.all?(board, fn(x) -> Regex.match?(~r/^\*+$/, x) end) -> board
-      #if there are no bombs on the field then we don't need to do anything
-      Enum.all?(board, fn(x) -> Regex.match?(~r/^\s+$/, x)  end) -> board
-      true -> process_board(board)
-    end
+    process_board(
+      not(Enum.all?(board, fn(x) -> Regex.match?(~r/^\*+$/, x) end) or 
+          Enum.all?(board, fn(x) -> Regex.match?(~r/^\s+$/, x)  end)),
+      board)
   end
 
-  defp process_board(board) do
+  defp process_board(false, board), do: board
+  defp process_board(true, board) do
     map = board |> board_to_map
-
-    bombs = map |> Enum.filter(fn({_key, v}) -> v == "*" end)
-    map = bombs |> annotated_map(map)
-
-    map |> map_to_board(length(board))
+    map
+     |> Enum.filter(fn({_key, v}) -> v == "*" end)
+     |> annotated_map(map)
+     |> map_to_board(length(board))
   end
 
-  defp board_to_map(board),
-    do: board 
-          |> Enum.with_index 
-          |> Enum.reduce(%{},
-              fn({row, indX}, acc) -> 
-                Enum.reduce(
-                  Enum.with_index(String.graphemes(row)),
-                  acc,
-                  fn({char, indY}, acc) -> Map.put(acc, {indX, indY}, char) end)
-              end)
+  defp board_to_map(board) do
+      board 
+        |> Enum.with_index 
+        |> Enum.reduce(%{},
+            fn({row, indX}, acc) -> 
+              Enum.reduce(
+                Enum.with_index(String.graphemes(row)),
+                acc,
+                fn({char, indY}, acc) -> Map.put(acc, {indX, indY}, char) end)
+            end)
+    end
 
   defp annotated_map([], map), do: map
   defp annotated_map([{{x, y}, _v} | other_bombs], map) do
@@ -48,7 +46,7 @@ defmodule Minesweeper do
   defp neighbours(x, y) do
     for i <- -1..1, 
         j <- -1..1, 
-        i != 0 or j != 0,
+        {i, j} != 0,
         do: {x + i, y + j}
   end
 
@@ -58,15 +56,17 @@ defmodule Minesweeper do
   defp replace_char(char), do: Integer.to_string(String.to_integer(char) + 1)
 
   #recreate board as list of strings from the map
-  defp map_to_board(map, board_length),
-    do: Enum.reduce(
-          Enum.to_list(0..board_length - 1),
-          [],
-          fn(ind, acc) -> acc ++ [create_row_from_map(map, ind)] end)
+  defp map_to_board(map, board_length) do
+    Enum.reduce(
+      Enum.to_list(0..board_length - 1),
+      [],
+      fn(ind, acc) -> acc ++ [create_row_from_map(map, ind)] end)
+  end
 
-  defp create_row_from_map(map, ind),
-    do: map |> Enum.filter(fn({{x, _y}, _v}) -> x == ind end)
-            |> Enum.map(fn({_key, v}) -> v end)
-            |> Enum.join("")
+  defp create_row_from_map(map, ind) do
+    map |> Enum.filter(fn({{x, _y}, _v}) -> x == ind end)
+        |> Enum.map(fn({_key, v}) -> v end)
+        |> Enum.join("")
+  end
 
 end
