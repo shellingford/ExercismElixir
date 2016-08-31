9e37af1f5e67e808447a12b3f3ae280c5312f609
Renn <rbiggy@gmail.com>
2016-08-31 14:08:28 +0200

    Remade Minesweeper to be a little more "functional"

--- minesweeper/minesweeper.exs
+++ minesweeper/minesweeper.exs
@@ -4,72 +4,69 @@ defmodule Minesweeper do
   Annotate empty spots next to mines with the number of mines next to them.
   """
   @spec annotate([String.t]) :: [String.t]
-
   def annotate(board) do
-  	cond do
-  		length(board) == 0 -> board
-      #if there are only bombs on the board then we don't need to do anything
-  		Enum.all?(board, fn(x) -> Regex.match?(~r/^\*+$/, x) end) -> board
+    cond do
+      length(board) == 0 -> board
+      #if there's only bombs on the board then we don't need to do anything
+      Enum.all?(board, fn(x) -> Regex.match?(~r/^\*+$/, x) end) -> board
       #if there are no bombs on the field then we don't need to do anything
-  		Enum.all?(board, fn(x) -> Regex.match?(~r/^\s+$/, x)  end) -> board
-  		true -> process_board(board)
-  	end
+      Enum.all?(board, fn(x) -> Regex.match?(~r/^\s+$/, x)  end) -> board
+      true -> process_board(board)
+    end
   end
 
   defp process_board(board) do
-  	no_rows = length(board) - 1
-  	no_cols = String.length(Enum.at(board, 0)) - 1
+    map = board |> board_to_map
 
-  	coord = for x <- 0..no_rows, y <- 0..no_cols, do: {x, y}
-  	process_elements(coord, no_rows, no_cols, board)
+    bombs = map |> Enum.filter(fn({_key, v}) -> v == "*" end)
+    map = bombs |> annotated_map(map)
+
+    map |> map_to_board(length(board))
   end
 
-  defp process_elements([], _no_rows, _no_cols, board), do: board
-  defp process_elements([{x, y} | coords], no_rows, no_cols, board) do
-  	row = Enum.at(board, x)
-  	char = String.at(row, y)
-  	board = change_board(char, x, y, no_rows, no_cols,board)
-  	process_elements(coords, no_rows, no_cols, board)
+  defp board_to_map(board),
+    do: board 
+          |> Enum.with_index 
+          |> Enum.reduce(%{},
+              fn({row, indX}, acc) -> 
+                Enum.reduce(
+                  Enum.with_index(String.graphemes(row)),
+                  acc,
+                  fn({char, indY}, acc) -> Map.put(acc, {indX, indY}, char) end)
+              end)
+
+  defp annotated_map([], map), do: map
+  defp annotated_map([{{x, y}, _v} | other_bombs], map) do
+    neighbours = neighbours(x, y)
+    map = map
+      |> Enum.filter(fn({coords, _v}) -> coords in neighbours end)
+      |> Enum.reduce(map, fn({key, _v}, acc) -> Map.update!(acc, key, &replace_char/1) end)
+
+    annotated_map(other_bombs, map)
   end
 
-  defp change_board("*", x, y, no_rows, no_cols, board), do: update_surrounding_elems(x, y, no_rows, no_cols, board)
-  defp change_board(_char, _x, _y, _no_rows, _no_cols, board), do: board
-
-  defp update_surrounding_elems(x, y, no_rows, no_cols, board) do
-  	board = increase_number(x, y + 1, no_rows, no_cols, board)
-  	board = increase_number(x, y - 1, no_rows, no_cols, board)
-
-  	board = increase_number(x + 1, y, no_rows, no_cols, board)
-  	board = increase_number(x - 1, y, no_rows, no_cols, board)
-
-  	board = increase_number(x + 1, y + 1, no_rows, no_cols, board)
-  	board = increase_number(x + 1, y - 1, no_rows, no_cols, board)
-
-  	board = increase_number(x - 1, y + 1, no_rows, no_cols, board)
-  	increase_number(x - 1, y - 1, no_rows, no_cols, board)
+  defp neighbours(x, y) do
+    for i <- -1..1, 
+        j <- -1..1, 
+        i != 0 or j != 0,
+        do: {x + i, y + j}
   end
 
-  defp increase_number(x, y, no_rows, no_cols, board) when x >= 0 and x <= no_rows and y >= 0 and y <= no_cols do
-  	row = Enum.at(board, x)
-  	char = String.at(row, y)
-
-  	char = replace_char(char)
-	  row = row
-  		|> String.graphemes
-  		|> List.replace_at(y, char)
-  		|> Enum.join("")
-
-	  List.replace_at(board, x, row)
-  end
-  defp increase_number(_x, _y, _no_rows, _no_cols, board), do: board
-
+  #replaces current char with number of mines
   defp replace_char(" "), do: "1" 
   defp replace_char("*"), do: "*"
-  defp replace_char(char) do
-  	if Regex.match?(~r/[1-7]{1}/, char) do
-  		Integer.to_string(String.to_integer(char) + 1)
-  	else
-  		char
-  	end
-  end
+  defp replace_char(char), do: Integer.to_string(String.to_integer(char) + 1)
+
+  #recreate board as list of strings from the map
+  defp map_to_board(map, board_length),
+    do: Enum.reduce(
+          Enum.to_list(0..board_length - 1),
+          [],
+          fn(ind, acc) -> acc ++ [create_row_from_map(map, ind)] end)
+
+  defp create_row_from_map(map, ind),
+    do: map |> Enum.filter(fn({{x, _y}, _v}) -> x == ind end)
+            |> Enum.map(fn({_key, v}) -> v end)
+            |> Enum.join("")
+
 end
